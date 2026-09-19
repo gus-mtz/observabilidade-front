181de7c960cb750e7d0207a902969da3c94c8770
gus-mtz <100966791+gus-mtz@users.noreply.github.com>
2026-09-19 11:02:38 -0300

    Change mtz_obs from string to array

--- obs.js
+++ obs.js
@@ -1,13 +1,15 @@
 (function() {
-  window.mtz_obs = window.mtz_obs || '';
+  window.mtz_obs = window.mtz_obs || [];
   var url = "https://bq-collector.dev.i.metricaz.com/collect";
   var xhr = new XMLHttpRequest();
-
+  mtz_obs.ts = Date.now();
+  
   xhr.open("POST", url, true);
 
   xhr.setRequestHeader(
     "Content-Type",
     "application/json;charset=UTF-8"
   );
+  
   xhr.send(JSON.stringify(mtz_obs));
 })();
